1a87267f52655024938eb53208fb5bea148f35ff
JaungiMein <itsherchoice101.0@gmail.com>
2025-08-06 21:05:23 +0000

    done

--- bot.py
+++ bot.py
@@ -614,7 +614,7 @@ async def channel_search_callback_handler(client, callback_query: CallbackQuery)
             f"<b>🛒 Category:</b> {channel_name}\n"
             f"<b>❌ No files found</b>.\n\n"            
             f"📝 <i>Tip: Double-check your spelling or try searching the title on <a href='https://www.google.com/search?q={quote_plus(query)}'>Google</a>.</i>"
-            f"<b>❓ What's available? Check <a href='{UPDATE_CHANNEL_LINK}'>here</a>.</b>"
+            f"<b>❓ What's available? Check <a href='{UPDATE_CHANNEL_LINK}'>here</a>.</b>",
             parse_mode=enums.ParseMode.HTML,
             disable_web_page_preview=True
         )

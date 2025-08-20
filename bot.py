704da401304c4c7bdf937b12f3154191ec45b4b3
JaungiMein <itsherchoice101.0@gmail.com>
2025-08-20 15:37:10 +0000

    workers change

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ bot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    workers=1000,
+    workers=4,
     parse_mode=enums.ParseMode.HTML
 )
 
@@ -613,7 +613,7 @@ async def channel_search_callback_handler(client, callback_query: CallbackQuery)
             f"<b>🕵🏻‍♂️ Query: <code>{query}</b></code>\n"
             f"<b>🛒 Category:</b> {channel_name}\n"
             f"<b>❌ No files found</b>.\n\n"            
-            f"📝 <i>Tip: Double-check your spelling or try searching the title on <a href='https://www.google.com/search?q={quote_plus(query)}'>Google</a>.</i>\n"
+            f"📝 <i>Tip: Double-check your spelling or try searching the title on <a href='https://www.google.com/search?q={quote_plus(query)}'>Google</a>.</i>\n\n"
             f"<b>❓ What's available? Check <a href='{UPDATE_CHANNEL_LINK}'>here</a>.</b>",
             parse_mode=enums.ParseMode.HTML,
             disable_web_page_preview=True

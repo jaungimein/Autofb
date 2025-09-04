ae249b0f1a3788a06035e9915bed3441ead5e973
JaungiMein <itsherchoice101.0@gmail.com>
2025-09-04 12:25:00 +0000

    pipline search enhancement

--- utility.py
+++ utility.py
@@ -74,9 +74,10 @@ def build_search_pipeline(query, allowed_ids, skip, limit):
     # Create a separate `text` clause for each term
     must_clauses = [
         {
-            "text": {
-                "query": term,
-                "path": "file_name"
+            "wildcard": {
+                "query": f"*{term}*",
+                "path": "file_name",
+                "allowAnalyzedField": True
             }
         }
         for term in terms

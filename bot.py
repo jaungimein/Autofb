f2aebb2e1abb8deee453166739efe7ec4bf5fe59
JaungiMein <itsherchoice101.0@gmail.com>
2025-09-16 15:26:17 +0000

    fx

--- bot.py
+++ bot.py
@@ -197,10 +197,8 @@ async def start_handler(client, message):
             keyboard = [buttons[i:i+2] for i in range(0, len(buttons)-1, 2)] + [[buttons[-1]]]
 
             welcome_text = (
-                f"<b>Hey {first_name}!</b> 👋\n"
-                f"<b>I'm Auto Filter 🤖</b>\n"
-                f"<b>Type below 👇 to 🔍</b>\n"
-                f"<b>what’s available ❓ Tap 👇</b>\n"
+                f"<b>Hey {first_name} I'm Auto Filter 🤖!</b>\n"
+                f"<b>Type below 👇 to 🔍 or Tap 👇</b>\n"
                 f"<b><i>Joined on {joined_str}</i></b> 🎉"
             )
 

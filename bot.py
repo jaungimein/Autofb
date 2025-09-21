53840778e5a686c8f6f53c2a976030a091aae6af
JaungiMein <itsherchoice101.0@gmail.com>
2025-09-21 21:19:31 +0000

    fx

--- bot.py
+++ bot.py
@@ -960,28 +960,33 @@ async def view_file_callback_handler(client, callback_query: CallbackQuery):
     
     file_name = file_doc.get("file_name", "Unknown file")
     ss_url = file_doc.get("ss_url", None)
-    results, tmdb_id, type= await get_info_by_name(file_name, channel_id)
-    if results:
-        trailer = results.get('trailer_url')
-        keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🎥 Trailer", url=trailer)]]) if trailer else None
+    try:
+        results, tmdb_id, type= await get_info_by_name(file_name, channel_id)
+        if results:
+            trailer = results.get('trailer_url')
+            keyboard = InlineKeyboardMarkup(
+                [[InlineKeyboardButton("🎥 Trailer", url=trailer)]]) if trailer else None
 
-        reply = await bot.send_photo(chat_id=callback_query.from_user.id,
-                             photo=results.get('poster_url'),
-                             caption=f"{results.get('message')}\n\n{file_name}",
-                             reply_markup=keyboard
-                             )
-    elif ss_url:
-        reply = await bot.send_message(chat_id=callback_query.from_user.id,
-                        text=f'<a href="{ss_url}">{file_name}</a>',
-                        protect_content=True
-                        )
-    else:
-        await callback_query.answer(f"{file_name}", show_alert=True)
-    
-    if reply:
-            bot.loop.create_task(delete_after_delay(reply))
-    
+            reply = await bot.send_photo(chat_id=callback_query.from_user.id,
+                                photo=results.get('poster_url'),
+                                caption=f"{results.get('message')}\n\n{file_name}",
+                                reply_markup=keyboard
+                                )
+        elif ss_url:
+            reply = await bot.send_message(chat_id=callback_query.from_user.id,
+                            text=f'<a href="{ss_url}">{file_name}</a>',
+                            protect_content=True
+                            )
+        else:
+            await callback_query.answer(f"{file_name}", show_alert=True)
+
+        await callback_query.answer()
+        
+        if reply:
+                bot.loop.create_task(delete_after_delay(reply))
+    except Exception as e:
+        logger.error(f"Error in view_file_callback_handler {e}")
+        await callback_query.answer()
         
 
 
